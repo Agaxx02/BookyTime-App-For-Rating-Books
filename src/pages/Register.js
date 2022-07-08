e58c3c204455036301b634f4d2210957d43507c0
agnieszka <agaxx02@gmail.com>
2022-07-08 13:14:26 +0200

    Register fetch done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,29 +3,38 @@ import React, { useState } from 'react'
 export default function Register() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [confirmPassowrd, setConfirmPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
 
 
-    const register = () => {
+    const register = (e) => {
+      e.preventDefault();
         fetch(`http://localhost:8000/register`, {
-            
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username,
+              password,
+              confirmPassword
+            })
         })
     }
 
   return (
     <div>
         <h1>Register</h1>
-        <form>
+        <form onSubmit={register}>
             <label htmlFor='username'>Username</label>
             <input id="username" onChange={(e) => setUsername(e.target.value)}></input>
             <br/>
             <label htmlFor='password'>Password</label>
             <input onChange={(e) => setPassword(e.target.value)} type='password'  id="password"></input>
             < br/>
-            <label htmlFor='confirmPassowrd'>Confirm password</label>
+            <label htmlFor='confirmPassword'>Confirm password</label>
             <input onChange={(e) => setConfirmPassword(e.target.value)} type='password'  id="confirmPassword"></input>
             <br />
-            <button onClick={register}>Register</button>
+            <button type='submit'>Register</button>
         </form>
     </div>
   )
