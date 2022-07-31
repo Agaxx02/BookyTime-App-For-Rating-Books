5802bade6137d90eac7a5672a7277fea37f21244
agnieszka <agaxx02@gmail.com>
2022-07-31 12:27:53 +0200

    Bug with credentials

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './App.css';
 import Login from './pages/Login';
 import Welcome from './pages/Welcome';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Library from './pages/Library';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export const CredentialsContext = React.createContext()
+export const CredentialsContext = React.createContext();
 
 function App() {
-  const credentialsState = useState(null)
- 
-  return (
-    <div className="App">
-      <CredentialsContext.Provider value={credentialsState}>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Welcome />} />
-        <Route path='register' element={<Register />} />
-        <Route path='login' element={<Login />} />
-        <Route path='dashboard' element={<Dashboard />} />
-        <Route path='library' element={<Library />} />
-      </Routes>
-      </BrowserRouter>
-      </CredentialsContext.Provider>
-    </div>
-  );
+	const credentialsState = useState(null);
+
+	return (
+		<div className='App'>
+			<CredentialsContext.Provider value={credentialsState}>
+				<BrowserRouter>
+					<Routes>
+						<Route path='/' element={<Welcome />} />
+						<Route path='register' element={<Register />} />
+						<Route path='login' element={<Login />} />
+						<Route path='dashboard' element={<Dashboard />} />
+						<Route path='library' element={<Library />} />
+					</Routes>
+				</BrowserRouter>
+			</CredentialsContext.Provider>
+		</div>
+	);
 }
 
 export default App;
