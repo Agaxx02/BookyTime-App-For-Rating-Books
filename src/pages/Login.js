4316aa68bd0b61ed79f6660112e634c98c4a778e
agnieszka <agaxx02@gmail.com>
2022-08-03 15:25:30 +0200

    Adding styles WIp

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,50 +1,61 @@
-import React, { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CredentialsContext } from '../App';
 import { handleErrors } from './Register';
 
 export default function Login() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const navigate = useNavigate()  
-    const [credentials, setCredentials] = useContext(CredentialsContext)
-  
-    const login = (e) => {
-      e.preventDefault();
-        fetch(`http://localhost:8000/login`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              username,
-              password,
-            })
-        })
-        .then(handleErrors)
-        .then(() => {
-          setCredentials({username, password})
-          navigate('/dashboard')
-        })
-        .catch((error) => {
-          setError(error.message)
-        })
-    }
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+	const navigate = useNavigate();
+	const [credentials, setCredentials] = useContext(
+		CredentialsContext
+	);
 
-  return (
-    <div>
-        <h1>Login</h1>
-        {error && <span className='errorMessage'>{error}</span>}
-        <form onSubmit={login}>
-            <label htmlFor='username'>Username</label>
-            <input id="username" onChange={(e) => setUsername(e.target.value)}></input>
-            <br/>
-            <label htmlFor='password'>Password</label>
-            <input onChange={(e) => setPassword(e.target.value)} type='password'  id="password"></input>
-            < br/>
-            <button type='submit'>Login</button>
-        </form>
-    </div>
-  )
+	const login = (e) => {
+		e.preventDefault();
+		fetch(`http://localhost:8000/login`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username,
+				password,
+			}),
+		})
+			.then(handleErrors)
+			.then(() => {
+				setCredentials({ username, password });
+				navigate('/dashboard');
+			})
+			.catch((error) => {
+				setError(error.message);
+			});
+	};
+
+	return (
+		<div className='login'>
+			<h1>Login</h1>
+			{error && <span className='errorMessage'>{error}</span>}
+			<form onSubmit={login}>
+				<label htmlFor='username'>Username</label>
+				<input
+					id='username'
+					onChange={(e) => setUsername(e.target.value)}
+				></input>
+				<br />
+				<label htmlFor='password'>Password</label>
+				<input
+					onChange={(e) => setPassword(e.target.value)}
+					type='password'
+					id='password'
+				></input>
+				<br />
+				<button type='submit' className='button'>
+					Login
+				</button>
+			</form>
+		</div>
+	);
 }
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Welcome() {
 	return (
 		<div className='welcome'>
-			<h1>Welcome</h1>
+			<h1>Welcome!</h1>
 			<Link to='/login' className='button link'>
 				Login
 			</Link>
