b70fcc43a2ae177d5ec4a4f40b23f54265dbdb29
agnieszka <agaxx02@gmail.com>
2022-09-10 19:18:01 +0200

    Added more detailed info and styled it a little

--- src/components/DetailedInfo.js
+++ src/components/DetailedInfo.js
@@ -1,26 +1,48 @@
 import React from 'react';
 
 export default function DetailedInfo(props) {
-	console.log(props.book);
 	return (
 		<div className='popup'>
-			<div className='popup-inner '>
-				<h2>Title: {props.book.title}</h2>
-				<h2>Author: {props.book.author_name}</h2>
-				<h2>ISBN Number: {props.book.isbn[0]}</h2>
-				<h2>Number of pages: {props.book.number_of_pages_median}</h2>
-				<img
-					className='cover'
-					alt='cover'
-					src={`https://covers.openlibrary.org/b/id/${props.book.cover_i}-M.jpg`}
-				></img>
-				<button
-					onClick={() => {
-						props.close(false);
-					}}
-				>
-					Close
-				</button>
+			<div className='popup-inner book '>
+				<section className='cover'>
+					<img
+						className='cover'
+						alt='cover'
+						src={`https://covers.openlibrary.org/b/id/${props.book.cover_i}-M.jpg`}
+					></img>
+				</section>
+				<section className='info'>
+					<h2>Title: {props.book.title}</h2>
+					<h2>Author: {props.book.author_name}</h2>
+					<h2>ISBN Number: {props.book.isbn[0]}</h2>
+					<h2>
+						Number of pages:
+						{props.book.number_of_pages_median
+							? props.book.number_of_pages_median
+							: 'unknown'}
+					</h2>
+					<h2>Publisher: {props.book.publisher}</h2>
+					<h2>
+						Published: {props.book.publish_place},
+						{props.book.publish_date}
+					</h2>
+					<h2>
+						Subjects:{' '}
+						{props.book.subject_facet
+							? props.book.subject_facet + ', '
+							: 'unknown'}
+					</h2>
+				</section>
+				<section className='editButtons'>
+					<button
+						className='smallerButton'
+						onClick={() => {
+							props.close(false);
+						}}
+					>
+						Close
+					</button>
+				</section>
 			</div>
 		</div>
 	);
